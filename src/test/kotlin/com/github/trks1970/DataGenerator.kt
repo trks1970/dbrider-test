4a2c725943a7888c8d3e522ef84b781305b3fb98
Tibor.Kiss <tibor.kiss.extern@porrsche.digital>
2022-07-30 16:30:11 +0200

    commit

--- src/test/kotlin/com/github/trks1970/DataGenerator.kt
+++ src/test/kotlin/com/github/trks1970/DataGenerator.kt
@@ -4,13 +4,10 @@ import com.github.database.rider.core.api.configuration.DBUnit
 import com.github.database.rider.core.api.dataset.DataSetFormat
 import com.github.database.rider.core.api.exporter.ExportDataSet
 import com.github.database.rider.junit5.api.DBRider
-import com.github.trks1970.EmailEntity
-import com.github.trks1970.EmailRepository
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
